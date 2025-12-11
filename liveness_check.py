45822d8a82b6af36357e8371e8f687b7db3b13e7
Antonios Hans <hanbizmail@gmail.com>
2025-12-12 00:18:30 +0700

    Add liveness checking

---
+++ liveness_check.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.efficientnet import preprocess_input
+from tensorflow.keras.layers import Layer
+import numpy as np
+import matplotlib.pyplot as plt
+
+IMG_SIZE = (224, 224)
+
+class CastLayer(Layer):
+    def call(self, inputs):
+        return tf.cast(inputs, tf.float32)
+
+custom_objects = {"Cast": CastLayer}
+
+model = load_model(
+    "./Models/14_model.h5",
+    custom_objects=custom_objects
+)
+
+def check_liveness(face):
+
+    img = tf.image.resize(face, IMG_SIZE)
+    img = tf.cast(img, tf.float32)
+    img = preprocess_input(img)
+    img = tf.expand_dims(img, axis=0)
+
+    pred = model.predict(img)[0][0]
+    label = int(pred > 0.5)
+    print(f"Pred: {pred}, Label: {label}")
+
+    return label
+
